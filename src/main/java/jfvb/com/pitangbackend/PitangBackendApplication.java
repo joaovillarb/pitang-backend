36f647f5c42954f1a06088a5d1274b54fdeb2426
joaofagundesvillar <joaovillarb@gmail.com>
2023-09-19 19:54:59 -0300

    initial commit

---
+++ src/main/java/jfvb/com/pitangbackend/PitangBackendApplication.java
@@ -0,0 +1,13 @@
+package jfvb.com.pitangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jfvb/com/pitangbackend/PitangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jfvb.com.pitangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
